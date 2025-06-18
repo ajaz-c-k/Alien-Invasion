43f0f28fe1114cd6425fd3cee7e671334b85bd69
Ajaz <ajazahammedck@gmail.com>
2025-06-18 14:08:38 +0530

    making Aliens move

--- settings.py
+++ settings.py
@@ -11,7 +11,10 @@ class Settings:
 
         #Ship settings
         self.ship_speed=1.5
-        
+        #alien settings
+        self.alien_speed=1.0
+
+        #bullet settings
         self.bullet_speed=1.0
         self.bullet_width=3
         self.bullet_height=15
