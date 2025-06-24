94c0ab72188e657e95b974c794f27d2296bdb41d
Ajaz <ajazahammedck@gmail.com>
2025-06-24 13:45:30 +0530

    Adding the play button

--- alien_invasion.py
+++ alien_invasion.py
@@ -6,6 +6,7 @@ from game_stats import GameStats
 from ship import Ship
 from bullet import Bullet
 from alien import Alien
+from button import Button
 
 
 class AlienInvasion:
@@ -36,6 +37,9 @@ class AlienInvasion:
         # Create the initial alien fleet
         self._create_fleet()
 
+        # Make the Play button.
+        self.play_button = Button(self, "Play")
+
     def run_game(self):
         """Start the main loop for the game."""
         while True:
@@ -55,6 +59,13 @@ class AlienInvasion:
                 self._check_keydown_events(event)
             elif event.type == pygame.KEYUP:
                 self._check_keyup_events(event)
+            elif event.type==pygame.MOUSEBUTTONDOWN:
+                mouse_pos=pygame.mouse.get_pos()
+                self._check_play_button(mouse_pos)
+    def _check_play_button(self, mouse_pos):
+        """Start a new game when the player clicks Play."""
+        if self.play_button.rect.collidepoint(mouse_pos):
+            self.stats.game_active = Tru
 
     def _check_keydown_events(self, event):
         """Respond to keypresses."""
@@ -132,10 +143,19 @@ class AlienInvasion:
         """Update the screen with current game objects."""
         self.screen.fill(self.settings.bg_color)
         self.ship.blitme()
+        
         for bullet in self.bullets.sprites():
             bullet.draw_bullet()
+        
         self.aliens.draw(self.screen)
+
+        # Draw the play button if the game is inactive.
+        if not self.stats.game_active:
+            self.play_button.draw_button()
+
+        # This must be LAST
         pygame.display.flip()
+
 
     def _update_aliens(self):
         """Update the positions of all aliens in the fleet."""
--- tempCodeRunnerFile.py
+++ tempCodeRunnerFile.py
@@ -1,40 +1,15 @@
-import pygame
-
-class Ship:
-
-    def __init__(self,ai_game):
-        """Initialise the Ship and its strating position"""
-        self.screen=ai_game.screen
-        self.settings=ai_game.settings
-        self.screen_rect=ai_game.screen.get_rect()
-
-        #load the ship image and get its rect
-        self.image=pygame.image.load('images/ship.bmp')
-        self.rect=self.image.get_rect()
-
-        #store the decimal value of ship in the horizontal position
-        self.x=float(self.rect.x)
-
-        #start each new ship at the centre of screen
-        self.rect.midbottom=self.screen_rect.midbottom
-
-        #Movement flag
-        self.moving_right=False
-        self.moving_left=False
-
-    def update(self):
-        """Update the ship's position based on the movemnet flag."""
-        if self.moving_right and self.rect.right<self.screen_rect.right:
-            self.x+=self.settings.ship_speed
-        if self.moving_left and self.rect.left>0:
-            self.x-=self.settings.ship_speed
+    """Update the screen with current game objects."""
+        self.screen.fill(self.settings.bg_color)
+        self.ship.blitme()
         
-        self.rect.x=self.x
+        for bullet in self.bullets.sprites():
+            bullet.draw_bullet()
+        
+        self.aliens.draw(self.screen)
 
-    def blitme(self):
-        """draw the ship at its current location"""
-        self.screen.blit(self.image,self.rect)
-    def center_ship(self):
-        """Center the ship on the screen."""
-        self.rect.midbottom = self.screen_rect.midbottom
-        self.x = float(self.rect.x)
+        # Draw the play button if the game is inactive.
+        if not self.stats.game_active:
+            self.play_button.draw_button()
+
+        # This must be LAST
+        pygame.display.flip()
